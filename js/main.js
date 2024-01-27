471f065872cc88a127f8261f306fe3406e52a2e9
Juhyun Min <xxo3082@naver.com>
2024-01-27 16:31:19 +0900

    feat: add search bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// BASKET
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -25,3 +26,44 @@ const showBasket = () => {
 const hideBasket = () => {
   basketEl.classList.remove("show");
 };
+
+// SEARCH
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchInputEl = searchWrapEl.querySelector("input");
+const searchDelayEl = [...searchWrapEl.querySelectorAll("li")];
+
+searchStarterEl.addEventListener("click", showSearch);
+searchCloserEl.addEventListener("click", hideSearch);
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEl.forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEl.length + "s";
+  });
+  setTimeout(() => {
+    searchInputEl.focus();
+  }, 600);
+}
+
+function hideSearch() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEl.reverse().forEach((el, index) => {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEl.length + "s";
+  });
+  searchDelayEl.reverse();
+  searchInputEl.value = ''
+}
